7c69c2ccbefed81fc920a215c7d66269fa5952be
Matt Burnett <mburnett@tutanota.de>
2021-11-29 23:24:13 +0100

    comm'd out swagger stuff in app.js to just get this to run on heroku

--- app.js
+++ app.js
@@ -14,10 +14,12 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 // stuff for swagger
-const swaggerUi = require('swagger-ui-express');
-const yaml = require('js-yaml');
-const fs = require('fs'); 
-const swaggerDocument = yaml.load(fs.readFileSync(path.resolve(__dirname, './swagger.yml'), 'utf8'));
+if(process.env.NODE_ENV !== 'production') {
+  const swaggerUi = require('swagger-ui-express');
+  const yaml = require('js-yaml');
+  const fs = require('fs'); 
+  const swaggerDocument = yaml.load(fs.readFileSync(path.resolve(__dirname, './swagger.yml'), 'utf8'));
+}
 
 // stuff for passport
 var bcrypt = require('bcrypt');
@@ -76,7 +78,9 @@ app.use('/api/v1/product', productRoutes);
 app.use('/api/v1/cart', cartRoutes);
 app.use('/api/v1/order', orderRoutes);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+if(process.env.NODE_ENV !== 'production') {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
