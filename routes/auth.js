9c39ab77a6636cf2b43134e194a92d9ab2a722e9
Matt Burnett <mburnett@tutanota.de>
2021-12-13 14:27:13 +0100

    took out oauth/google link so that we can run the 'try it out' functionality in swagger ui on heroku

--- routes/auth.js
+++ routes/auth.js
@@ -17,7 +17,7 @@ module.exports = (app) => {
 
     router.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
 
-    router.get('/auth/google/callback',  passport.authenticate('google', { // FIXME: works on localhost, but on prod it returns login screen
+    router.get('/auth/google/callback',  passport.authenticate('google', { // FIXME: works on localhost, but on prod it returns login screen in the 'try it out' section/s
         failureRedirect: '/login' 
     }), (req, res) => {
         res.redirect('/api-docs');
