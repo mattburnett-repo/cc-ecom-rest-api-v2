aefd214c96677d675135ca5ef20d20b13d990cb0
Matt Burnett <mburnett@tutanota.de>
2021-12-21 15:54:50 +0100

    /api/v1/auth/local is now functional, but messy. should use passport instead...

--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,16 @@ var router = express.Router();
 
 var passport = require('passport');
 
+const bcrypt = require('bcrypt');
+var db = require('../db');
+
 module.exports = (app) => {
     app.use('/', router);
     app.use('/login', router);
 
     router.get('/', (req, res) => res.render('login.ejs'));
 
+    // routes for auth from api server
     router.post('/', passport.authenticate('local', {
         successRedirect: '/api/v1/api-docs', // api-docs is the front end for the API project
         failureRedirect: '/login',
@@ -23,20 +27,52 @@ module.exports = (app) => {
         res.redirect('/api/v1/api-docs');
     });
 
-    router.get('/api/v1/auth/local', (req, res) => {
-        let msg = 'GET /api/v1/auth/local'
-        console.log({msg});
-        res.status(200).send({ message: msg})
-    })
-    router.get('/api/v1/auth/google', (req, res) => {
-        let msg = 'GET /api/v1/auth/google'
-        console.log({msg});
-        res.status(200).send({ message: msg})
-    })
-    router.get('/api/v1/auth/google/callback', (req, res) => {
-        let msg = 'GET /api/v1/auth/google/callback'
-        console.log({msg});
-        res.status(200).send({ message: msg})
-    })
+    // routes for auth from app server
+    // FIXME: this needs to be refactored into a model or something, and should use passport
+    // just copy local, above, and change path / redirects?
+    // router.post('/api/v1/auth/local', passport.authenticate('local', {
+    //     successRedirect: '/dashboard', 
+    //     failureRedirect: '/login',
+    //     failureFlash: true
+    // }));    
 
+    router.post('/api/v1/auth/local', async (req, res) => {
+        // body / header: application/x-www-form-urlencoded
+        const {username, password } = req.body
+
+        let msg = 'POST /api/v1/auth/local username: ' + username + ' password: ' + password
+        console.log({msg});
+
+        var theVals = [username]
+        var queryString = 'SELECT * FROM users WHERE user_name = $1'
+
+        const user = await db.query(queryString, theVals)
+
+        if(user.rowCount !== 1) {
+            res.status(401).send({ message: `User ${username} not found or not authorized` });
+            return;
+        }
+
+        try {   
+            if(await bcrypt.compare(password, user.rows[0].password)) {
+                res.status(200).send(user.rows);
+            } else {
+                res.status(401).send({ message: `user ${username} not authorized` });
+            }
+        } catch (e) {
+            res.status(400).send({ message: e.message });
+        }
+    }) // end post auth/local
+
+    // TODO: implement this, use passport
+    // router.get('/api/v1/auth/google', (req, res) => {
+    //     let msg = 'GET /api/v1/auth/google'
+    //     console.log({msg});
+    //     res.status(200).send({ message: msg})
+    // })
+    // router.get('/api/v1/auth/google/callback', (req, res) => {
+    //     let msg = 'GET /api/v1/auth/google/callback'
+    //     console.log({msg});
+    //     res.status(200).send({ message: msg})
+    // })
 }
